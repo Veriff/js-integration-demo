6d5a6f72653813e972c7431dba31cb6004514145
Andrus Asumets <andrus.asumets@gmail.com>
2018-06-27 11:37:00 +0300

    Add media POST, media GET, session end and a webhook listener

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
-# Readme
+## Veriff's JS integration demo  
+# Intro:  
+This is a barebones example on how to integrate Veriff's API in a headless mode that skips our frontend application.  
+It currently does five things for you:
+1. Starts a verification session with Veriff.  
+2. Uploads three images and attaches those to the verification session.  
+3. Ends the verification session.
+4. Does a media query for the verification session and receives the media that was uploaded during the second step.
+5. Optionally, it also receives a decision notification webhook from Veriff of the final decision, which usually shouldn't take longer than few minutes.
 
-## Add secret and key
-On line 7 and 8 of veriff_demo.js, please add your secret and key.
-```
-var clientId = 'Add your key here';
-var secret = 'Add your secret here';
-```
+# Installation:  
+`npm install`  
 
-## Install dependencies
-```
-cd /to/source/files
-npm install
-```
+# Run:  
+Get 'Api key' and 'Api secret' (under Management -> Vendor) and respectively fill in API_TOKEN and API_SECRET environment variables.  
 
-## Send your first test request
-```
-node
->  const veriffDemo = require('./veriff_demo.js')
-> veriffDemo.createSession()
-```
-Voila
+`API_TOKEN={_API_TOKEN} API_SECRET={API_SECRET} node app.js`  
+
+Optionally, for receiving decision notifications for testing purposes, update 'Web hook url' (under Management -> Vendor -> Edit), and then include a WEBHOOK_PORT={PORT} argument that will boot up an Express.js webserver, and opens an endpoint that starts listening to the decision notification webhook for you (assuming your service has a public IP and domain and is open to the internet).  
---
+++ app.js
@@ -0,0 +1,179 @@
+// Dependencies
+const fs = require('fs');
+const crypto = require('crypto');
+const fetch = require('node-fetch');
+
+// Globals
+const API_TOKEN = process.env.API_TOKEN;
+const API_SECRET = process.env.API_SECRET;
+const API_URL = process.env.API_URL || 'https://api.veriff.me/v1';
+const WEBHOOK_PORT = process.env.WEBHOOK_PORT;
+const IMAGE_DIR = process.env.IMAGE_DIR || 'documents';
+
+if (!API_TOKEN) throw('API_TOKEN environment variable is required');
+if (!API_SECRET) throw('API_SECRET environment variable is required');
+
+// Initialize
+(async () => {
+  try {
+    const files = fs.readdirSync('./' + IMAGE_DIR).filter(file => file.match(/.*\.(jpg|jpeg|png)/ig));
+    const session = await start();
+    const verificationId = session.verification.id;
+    console.log('Started verification with an id of', verificationId);
+
+    const uploads = files.map(async (file) => {
+      await upload(verificationId, file);
+      console.log('Uploaded file', file);
+    });
+
+    Promise.all(uploads).then(async () => {
+      await end(verificationId);
+      console.log('Ended verification');
+
+      console.log('Getting media for the verification session');
+      const media = await getMedia(verificationId);
+      console.log(JSON.stringify(media, null, 4));
+    });
+  }
+  catch(err) {
+    console.log(err);
+  }
+})()
+
+async function start() {
+  try {
+    const payload = {
+      verification: {
+        person: {
+          firstName: 'Tundmatu',
+          lastName: 'Toomas',
+          idNumber: '38508260269'
+        },
+        document: {
+          number: 'B01234567',
+          type: 'ID_CARD',
+          country: 'EE'
+        },
+        additionalData: {
+          citizenship: 'EE',
+          placeOfResidence: 'Tallinn'
+        },
+        lang: 'en',
+        features: ['selfid'],
+        timestamp: timestamp()
+      }
+    };
+
+    const headers = {
+      'x-auth-client': API_TOKEN,
+      'x-signature': generateSignature(payload, API_SECRET),
+      'content-type':'application/json'
+    };
+
+    const options = { method: 'POST', headers: headers, body: JSON.stringify(payload) };
+    const response = await fetch(API_URL + '/sessions', options);
+    return await response.json();
+  }
+  catch(err) {
+    console.log('verify method error', err);
+  }
+}
+
+async function upload(verificationId, file) {
+  try {
+    const payload = {
+      image: {
+        context: file.split('.')[0],
+        content: readImage(IMAGE_DIR + '/' + file),
+        timestamp: timestamp()
+      }
+    };
+
+    const headers = {
+      'x-auth-client': API_TOKEN,
+      'x-signature': generateSignature(payload, API_SECRET),
+      'content-type':'application/json'
+    };
+
+    const options = { method: 'POST', headers: headers, body: JSON.stringify(payload) };
+    const response = await fetch(API_URL + '/sessions/' + verificationId + '/media', options);
+    const json = await response.json();
+    return response;
+  }
+  catch(err) {
+    console.log('upload method error', err);
+  }
+}
+
+async function getMedia(verificationId) {
+  try {
+    const headers = {
+      'x-auth-client': API_TOKEN,
+      'x-signature': generateSignature(verificationId, API_SECRET)
+    };
+
+    const options = { method: 'GET', headers: headers };
+    const response = await fetch(API_URL + '/sessions/' + verificationId + '/media', options);
+    return await response.json();
+  }
+  catch(err) {
+    console.log('getMedia method error', err);
+  }
+}
+
+async function end(verificationId) {
+  try {
+    const payload = { verification: { frontState: 'done', status: 'submitted', timestamp: timestamp() } };
+
+    const headers = {
+      'x-auth-client': API_TOKEN,
+      'x-signature': generateSignature(payload, API_SECRET),
+      'content-type':'application/json'
+    };
+
+    const options = { method: 'PATCH', headers: headers, body: JSON.stringify(payload) };
+    const response = await fetch(API_URL + '/sessions/' + verificationId, options);
+    return await response.json();
+  }
+  catch(err) {
+    console.log('done method error', err);
+  }
+}
+
+function timestamp() { return new Date().toISOString() };
+
+function readImage(file) {
+  const bitmap = fs.readFileSync(file);
+  return Buffer(bitmap).toString('base64');
+}
+
+function generateSignature(payload, secret) {
+  if (payload.constructor === Object) {
+    payload = JSON.stringify(payload);
+  }
+
+  if (payload.constructor !== Buffer) {
+    payload = Buffer.from(payload, 'utf8');
+  }
+
+  const signature = crypto.createHash('sha256');
+  signature.update(payload);
+  signature.update(new Buffer(secret, 'utf8'));
+  return signature.digest('hex');
+}
+
+// Receive a web hook locally
+if (WEBHOOK_PORT) {
+  const express = require('express');
+  const app = express();
+  let server = require('http').Server(app);
+
+  app.post('/verification/', (req, res) => {
+    console.log('Received a webhook');
+    console.log(req.body);
+    res.json({ status: 'success' });
+    process.exit();
+  })
+
+  server.listen(WEBHOOK_PORT);
+}
